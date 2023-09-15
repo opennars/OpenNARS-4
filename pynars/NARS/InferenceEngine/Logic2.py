f4d3fd629dd6384409f0a3252a3062746f3ceeeb
Maxim <maxeeem@gmail.com>
2023-09-15 05:51:02 -0700

    adding variable elimination

--- pynars/NARS/InferenceEngine/Logic2.py
+++ pynars/NARS/InferenceEngine/Logic2.py
@@ -2,7 +2,7 @@ from kanren import run, eq, var
 from cons import cons, car, cdr
 
 from pynars import Narsese
-from pynars.Narsese import Term, Copula, Connector, Statement, Compound
+from pynars.Narsese import Term, Copula, Connector, Statement, Compound, Variable, VarPrefix
 
 
 #################################################
@@ -14,15 +14,29 @@ prefix = '_rule_'
 def logic(term: Term, rule=False):
     if term.is_atom:
         name = prefix+term.word if rule else term.word
-        return var(name)
+        if type(term) is Variable:
+            vname = term.word + term.name
+            name = prefix+vname if rule else vname 
+            return var(name)
+        return var(name) if rule else term
     if term.is_statement:
         return cons(term.copula, *[logic(t, rule) for t in term.terms])
     if term.is_compound:
         return cons(term.connector, *[logic(t, rule) for t in term.terms])
 
 def term(logic):
+    if type(logic) is Term:
+        return logic
     if type(logic) is var:
-        return Term(logic.token)
+        name = logic.token.replace(prefix, '')
+        if name[0] == '$':
+            return Variable(VarPrefix.Independent, name[1:])
+        if name[0] == '#':
+            return Variable(VarPrefix.Dependent, name[1:])
+        if name[0] == '?':
+            return Variable(VarPrefix.Query, name[1:])
+        else:
+            return Term(name)
     if type(logic) is cons:
         if type(car(logic)) is Copula:
             sub = car(cdr(logic))
@@ -31,7 +45,8 @@ def term(logic):
             return Statement(term(sub), cop, term(pre))
         if type(car(logic)) is Connector:
             con = car(logic)
-            terms = to_list(cdr(logic))
+            t = cdr(logic)
+            terms = to_list(cdr(logic)) if type(t) is cons else [term(t)]
             return Compound(con, *terms)
     return logic # cons
 
@@ -75,6 +90,24 @@ def diff(c):
     else:
         return Statement(difference, c.copula, c.predicate)
 
+# UNIFICATION
+
+from unification import unify, reify
+
+def variable_elimination(t1: Term, t2: Term) -> list:
+    unified = filter(None, (unify(logic(t), logic(t2, True)) for t in t1.terms))
+    substitution = []
+    for u in unified:
+        d = {k: v for k, v in u.items() if type(term(k)) is Variable}
+        if len(d):
+            substitution.append(d)
+    result = []
+    for s in substitution:
+        reified = reify(logic(t1), s)
+        result.append(term(reified))
+
+    return result
+
 #################################################
 
 
@@ -84,6 +117,9 @@ rule = '''{<(&&, C, S) ==> P>. M} |- <((&&, C, S) - M) ==> P>'''
 rules = [rule, '{<M-->P>. <S-->M>} |- <S-->P>']
 
 
+def parse(narsese: str) -> Term:
+    return Narsese.parser.parse(narsese).term
+
 def convert(rule):
     # convert to logical form
     premises, conclusion = rule.split(" |- ")
@@ -91,9 +127,9 @@ def convert(rule):
     p1, p2 = premises.strip("{}").split(". ")
 
     # TODO: can we parse statements instead?
-    p1 = Narsese.parser.parse(p1+'.').term
-    p2 = Narsese.parser.parse(p2+'.').term
-    c = Narsese.parser.parse(conclusion+'.').term
+    p1 = parse(p1+'.')
+    p2 = parse(p2+'.')
+    c = parse(conclusion+'.')
 
     p1 = logic(p1, True)
     p2 = logic(p2, True)
@@ -108,12 +144,23 @@ def apply(rule, *args):
     t1, t2 = args
     print("Test:", t1, t2)
 
+    # TODO: what about other possibilities?
+    t1e = variable_elimination(t1, t2)
+    t2e = variable_elimination(t2, t1)
+
+    t1 = t1e[0] if len(t1e) else t1
+    t2 = t2e[0] if len(t2e) else t2
+
+    if len(t1e) or len(t2e):
+        print("Substituted:", t1, t2)
+
     # apply rule
-    result = run(1, c, eq(p1, logic(t1)), eq(p2, logic(t2)))
+    result = run(1, c, eq((p1, p2), (logic(t1), logic(t2))))
     # print(result)
 
     if result:
         conclusion = term(result[0])
+        # print(conclusion)
         # apply diff connector
         difference = diff(conclusion)
         if difference == None:
@@ -129,23 +176,54 @@ def apply(rule, *args):
 
 # CONDITIONAL
 
-t1 = Narsese.parser.parse('<(&&, A, B, C, D) ==> Z>.').term
+t1 = parse('<(&&, A, B, C, D) ==> Z>.')
 
-t2 = Narsese.parser.parse('B.').term # positive example
+t2 = parse('B.') # positive example
 for rule in rules:
     apply(rule, t1, t2)
 
-t2 = Narsese.parser.parse('U.').term # negative example
+t2 = parse('U.') # negative example
 for rule in rules:
     apply(rule, t1, t2)
 
-t2 = Narsese.parser.parse('(&&, B, C).').term # complex example
+t2 = parse('(&&, B, C).') # complex example
 for rule in rules:
     apply(rule, t1, t2)
 
 # DEDUCTION
 
-t1 = Narsese.parser.parse('<bird --> animal>.').term
-t2 = Narsese.parser.parse('<robin --> bird>.').term
+t1 = parse('<bird --> animal>.')
+t2 = parse('<robin --> bird>.')
+for rule in rules:
+    apply(rule, t1, t2)
+
+print("\n\n----VARIABLE SUBSTITUTION")
+
+# CONDITIONAL SYLLOGISTIC
+
+rules = [
+    '{<S ==> P>. S} |- P', # deduction
+    '{<P ==> S>. S} |- P', #abduction
+    '{<(&&, C, S) ==> P>. M} |- <((&&, C, S) - M) ==> P>', # conditional ded
+    ]
+
+print('\n--nal6.7')
+t1 = parse('<<$x --> bird> ==> <$x --> animal>>.')
+t2 = parse('<robin --> bird>.')
+
+for rule in rules:
+    apply(rule, t1, t2)
+
+print('\n--nal6.8')
+t1 = parse('<<$x --> bird> ==> <$x --> animal>>.')
+t2 = parse('<tiger --> animal>.')
+
+for rule in rules:
+    apply(rule, t1, t2)
+
+print('\n--nal6.12')
+t1 = parse('<(&&,<$x --> flyer>,<$x --> [chirping]>, <(*, $x, worms) --> food>) ==> <$x --> bird>>.')
+t2 = parse('<{Tweety} --> flyer>.')
+
 for rule in rules:
     apply(rule, t1, t2)
