1b1eeb8ce416a00a87503fc551f3ec37599f4a08
Maxim <maxeeem@gmail.com>
2023-09-19 17:06:51 -0700

    clean up rule instantiation; add constraints ensuring variable uniqueness

--- pynars/NARS/InferenceEngine/KanrenEngine/KanrenEngine.py
+++ pynars/NARS/InferenceEngine/KanrenEngine/KanrenEngine.py
@@ -1,22 +1,55 @@
 from kanren import run, eq, var
+from kanren.constraints import neq
 from unification import unify, reify
 from cons import cons, car, cdr
+
+from itertools import combinations
 
 from pynars import Narsese
 from pynars.Narsese import Term, Copula, Connector, Statement, Compound, Variable, VarPrefix, Sentence
 
 from pynars.NAL.Functions.TruthValueFunctions import *
 
-def parse(narsese: str, rule=False):
-    task = Narsese.parser.parse(narsese)
-    return task.term if rule else task.sentence
 
-class KanrenEngine:
-    _all_rules = '''{<M --> P>. <S --> M>} |- <S --> P> .ded
+nal1 = '''
+{<M --> P>. <S --> M>} |- <S --> P> .ded
 {<P --> M>. <M --> S>} |- <P --> S> .ded'
-{<M ==> P>. <S ==> M>} |- <S ==> P> .ded
-{<P ==> M>. <M ==> S>} |- <P ==> S> .ded'
-{<S ==> P>. S} |- P .ded
+{<M --> P>. <M --> S>} |- <S --> P> .ind
+{<M --> P>. <M --> S>} |- <P --> S> .ind'
+{<P --> M>. <S --> M>} |- <S --> P> .abd
+{<P --> M>. <S --> M>} |- <P --> S> .abd'
+{<P --> M>. <M --> S>} |- <S --> P> .exe
+{<M --> P>. <S --> M>} |- <P --> S> .exe'
+'''
+
+nal2 = '''
+{<M <-> P>. <S <-> M>} |- <S <-> P> .res
+{<M <-> P>. <M <-> S>} |- <S <-> P> .res
+{<P <-> M>. <S <-> M>} |- <S <-> P> .res
+{<P <-> M>. <M <-> S>} |- <S <-> P> .res
+{<M --> P>. <M --> S>} |- <S <-> P> .com
+{<P --> M>. <S --> M>} |- <S <-> P> .com'
+{<M --> P>. <S <-> M>} |- <S --> P> .ana
+{<M --> P>. <M <-> S>} |- <S --> P> .ana
+{<P --> M>. <S <-> M>} |- <P --> S> .ana
+{<P --> M>. <M <-> S>} |- <P --> S> .ana
+{<M <-> P>. <S --> M>} |- <S --> P> .ana'
+{<P <-> M>. <S --> M>} |- <S --> P> .ana'
+{<M <-> P>. <M --> S>} |- <P --> S> .ana'
+{<P <-> M>. <M --> S>} |- <P --> S> .ana'
+'''
+
+nal3 = '''
+'composition
+{<M --> T1>. <M --> T2>} |- <M --> (&, T1, T2)> .int
+{<T1 --> M>. <T2 --> M>} |- <(|, T1, T2) --> M> .int
+{<M --> T1>. <M --> T2>} |- <M --> (|, T1, T2)> .uni
+{<T1 --> M>. <T2 --> M>} |- <(&, T1, T2) --> M> .uni
+{<M --> T1>. <M --> T2>} |- <M --> (-, T1, T2)> .dif
+{<M --> T1>. <M --> T2>} |- <M --> (-, T2, T1)> .dif'
+{<T1 --> M>. <T2 --> M>} |- <(~, T1, T2) --> M> .dif
+{<T1 --> M>. <T2 --> M>} |- <(~, T2, T1) --> M> .dif'
+'decomposition
 {(--, <M --> (&, T1, T2)>). <M --> T1>} |- (--, <M --> T2>) .ded
 {<M --> (|, T1, T2)>. (--, <M --> T1>)} |- <M --> T2> .ded
 {(--, <M --> (-, T1, T2)>). <M --> T1>} |- <M --> T2> .ded
@@ -27,72 +60,50 @@ class KanrenEngine:
 {(--, <(~, T2, T1) --> M>). (--, <T1 --> M>)} |- (--, <T2 --> M>) .ded
 {(--, (&&, T1, T2)). T1} |- (--, T2) .ded
 {(||, T1, T2). (--, T1)} |- T2 .ded
-{<M --> P>. <M --> S>} |- <S --> P> .ind
-{<M --> P>. <M --> S>} |- <P --> S> .ind'
-{<M ==> P>. <M ==> S>} |- <S ==> P> .ind
-{<M ==> P>. <M ==> S>} |- <P ==> S> .ind'
-{<P --> M>. <S --> M>} |- <S --> P> .abd
-{<P --> M>. <S --> M>} |- <P --> S> .abd'
-{<P ==> M>. <S ==> M>} |- <S ==> P> .abd
-{<P ==> M>. <S ==> M>} |- <P ==> S> .abd'
-{<P ==> S>. S} |- P .abd
-{<P --> M>. <M --> S>} |- <S --> P> .exe
-{<M --> P>. <S --> M>} |- <P --> S> .exe'
-{<P ==> M>. <M ==> S>} |- <S ==> P> .exe
-{<M ==> P>. <S ==> M>} |- <P ==> S> .exe'
-{<M --> P>. <M --> S>} |- <S <-> P> .com
-{<P --> M>. <S --> M>} |- <S <-> P> .com'
-{<M ==> P>. <M ==> S>} |- <S <=> P> .com
-{<P ==> M>. <S ==> M>} |- <S <=> P> .com'
-{<M --> P>. <S <-> M>} |- <S --> P> .ana
-{<P --> M>. <S <-> M>} |- <P --> S> .ana
-{<M <-> P>. <S --> M>} |- <S --> P> .ana'
-{<M <-> P>. <M --> S>} |- <P --> S> .ana'
-{<M ==> P>. <S <=> M>} |- <S ==> P> .ana
-{<P ==> M>. <S <=> M>} |- <P ==> S> .ana
-{<M <=> P>. <S ==> M>} |- <S ==> P> .ana'
-{<M <=> P>. <M ==> S>} |- <P ==> S> .ana'
-{S. <S <=> P>} |- P .ana
-{<M --> P>. <M <-> S>} |- <S --> P> .ana
-{<P --> M>. <M <-> S>} |- <P --> S> .ana
-{<P <-> M>. <S --> M>} |- <S --> P> .ana'
-{<P <-> M>. <M --> S>} |- <P --> S> .ana'
-{<M ==> P>. <M <=> S>} |- <S ==> P> .ana
-{<P ==> M>. <M <=> S>} |- <P ==> S> .ana
-{<P <=> M>. <S ==> M>} |- <S ==> P> .ana'
-{<P <=> M>. <M ==> S>} |- <P ==> S> .ana'
-{S. <P <=> S>} |- P .ana
-{<M <-> P>. <S <-> M>} |- <S <-> P> .res
-{<S --> P>. <P --> S>} |- <S <-> P> .res
-{<S --> P>. <P <-> S>} |- <P --> S> .res
-{<M <=> P>. <S <=> M>} |- <S <=> P> .res
-{<S ==> P>. <P ==> S>} |- <S <=> P> .res
-{<S ==> P>. <P <=> S>} |- <P ==> S> .res
-{<P <-> M>. <S <-> M>} |- <S <-> P> .res
-{<M <-> P>. <M <-> S>} |- <S <-> P> .res
-{<P <-> M>. <M <-> S>} |- <S <-> P> .res
-{<S --> P>. <S <-> P>} |- <P --> S> .res
-{<P <=> M>. <S <=> M>} |- <S <=> P> .res
-{<M <=> P>. <M <=> S>} |- <S <=> P> .res
-{<P <=> M>. <M <=> S>} |- <S <=> P> .res
-{<S ==> P>. <S <=> P>} |- <P ==> S> .res
-{<M --> T1>. <M --> T2>} |- <M --> (&, T1, T2)> .int
-{<T1 --> M>. <T2 --> M>} |- <(|, T1, T2) --> M> .int
-{<M ==> T1>. <M ==> T2>} |- <M ==> (&&, T1, T2)> .int
-{<T1 ==> M>. <T2 ==> M>} |- <(||, T1, T2) --> M> .int
-{<T1 --> T2>. <T2 --> T1>} |- (&&, <T1 --> T2>, <T2 --> T1>) .int
-{<M --> T1>. <M --> T2>} |- <M --> (|, T1, T2)> .uni
-{<T1 --> M>. <T2 --> M>} |- <(&, T1, T2) --> M> .uni
-{<M ==> T1>. <M ==> T2>} |- <M ==> (||, T1, T2)> .uni
-{<T1 ==> M>. <T2 ==> M>} |- <(&&, T1, T2) --> M> .uni
-{<T1 --> T2>. <T2 --> T1>} |- (||, <T1 --> T2>, <T2 --> T1>) .uni
-{<M --> T1>. <M --> T2>} |- <M --> (-, T1, T2)> .dif
-{<M --> T1>. <M --> T2>} |- <M --> (-, T2, T1)> .dif'
-{<T1 --> M>. <T2 --> M>} |- <(~, T1, T2) --> M> .dif
-{<T1 --> M>. <T2 --> M>} |- <(~, T2, T1) --> M> .dif'
-{<(&&, C, S) ==> P>. M} |- <((&&, C, S) - M) ==> P> .ded
-{<(&&, C, S) ==> P>. <X ==> M>} |- <((&&, C, S) - <X ==> M>) ==> P> .ded
 '''
+
+nal5 = '''
+'conditional syllogistic
+{<S ==> P>. S} |- P .ded
+{<P ==> S>. S} |- P .abd
+{S. <S <=> P>} |- P .ana
+{S. <P <=> S>} |- P .ana
+{<S <=> P>. S} |- P .ana’
+{<P <=> S>. S} |- P .ana’
+
+'conditional conjunctive
+'(C ^ S) => P, S |- C => P (alternative syntax below)
+{<(&&, C, S) ==> P>. _S} |- <((&&, C, S) - _S) ==> P> .ded
+
+'(C ^ S) => P, M => S |- (C ^ M) => P (alternative syntax below)
+{<(&&, C, S) ==> P>. <M ==> _S>} |- <(&&, ((&&, C, S) - _S), M) ==> P> .ded
+
+'(C ^ S) => P, C => P |- S (alternative syntax below)
+{<(&&, C, S) ==> P>. <_C ==> P>} |- ((&&, C, S) - _C) .abd
+
+'(C ^ S) => P, (C ^ M) => P |- M => S (alternative syntax below)
+{<(&&, C, S) ==> P>. <(&&, _C, M) ==> P>} |- <((&&, _C, M) - C) ==> ((&&, C, S) - _C)> .abd
+
+'{<C ==> P>. S} |- <(&&, C, S) ==> P> .ind (alternative syntax below)
+{<(&&, C, M) ==> P>. (&&, S, M)} |- <(&&, C, S, M) ==> P> .ind
+
+'(C ^ M) => P, M => S |- (C ^ S) => P (alternative syntax below)
+{<(&&, C, M) ==> P>. <_M ==> S>} |- <(&&, ((&&, C, M) - _M), S) ==> P> .ind
+'''
+
+def split_rules(rules: str) -> list[str]:
+    lines = []
+    for line in rules.splitlines():
+        if len(line) and not line.startswith("'"):
+            lines.append(line)
+    return lines
+
+def parse(narsese: str, rule=False):
+    task = Narsese.parser.parse(narsese)
+    return task.term if rule else task.sentence
+
+
+class KanrenEngine:
 
     _truth_functions = {
         'ded': Truth_deduction,
@@ -108,9 +119,47 @@ class KanrenEngine:
     }
 
     def __init__(self):
-        rules = self._all_rules.splitlines()
+        
+        nal1_rules = split_rules(nal1)
+        nal2_rules = split_rules(nal2)
+        nal3_rules = split_rules(nal3)
+
+        nal5_rules = split_rules(nal5)
+        
+        # NAL5 includes higher order variants of NAL1-3 rules
+        for rule in (nal1_rules + nal2_rules):
+            # replace --> with ==> in NAL1 & NAL2
+            rule = rule.replace('-->', '==>')
+            # replace <-> with <=> in NAL2
+            rule = rule.replace('<->', '<=>')
+
+            nal5_rules.append(rule)
+
+        for rule in nal3_rules:
+            # replace --> with ==> in NAL3 (except difference)
+            if '(-,' not in rule and '(~,' not in rule:
+                rule = rule.replace('-->', '==>')
+                
+                # replace | with || in NAL3 (except difference)
+                if '||' not in rule:
+                    parts = rule.split(' |- ')
+                    parts = (part.replace('|', '||') for part in parts)
+                    rule = ' |- '.join(parts)
+                
+                # replace & with && in NAL3 (except difference)
+                if '&&' not in rule:
+                    rule = rule.replace('&', '&&')
+                
+                nal5_rules.append(rule)
+        
+        rules = nal1_rules + nal2_rules + nal3_rules + nal5_rules
+
+        self.variables = set()
         self.rules = [self._convert(rule) for rule in rules]
 
+        var_combinations = list(combinations(self.variables, 2))
+        # TODO: allow ex. C and _C to be the same
+        self.constraints = [neq(c[0], c[1]) for c in var_combinations]
 
 
     #################################################
@@ -119,18 +168,22 @@ class KanrenEngine:
 
     _prefix = '_rule_'
 
-    def logic(self, term: Term, rule=False):
+    def logic(self, term: Term, rule=False, substitution=False):
         if term.is_atom:
             name = self._prefix+term.word if rule else term.word
             if type(term) is Variable:
                 vname = term.word + term.name
                 name = self._prefix+vname if rule else vname 
+                if rule and not substitution: # collect rule variable names
+                    self.variables.add(var(name))
                 return var(name)
+            if rule and not substitution: # collect rule variable names
+                self.variables.add(var(name))
             return var(name) if rule else term
         if term.is_statement:
-            return cons(term.copula, *[self.logic(t, rule) for t in term.terms])
+            return cons(term.copula, *[self.logic(t, rule, substitution) for t in term.terms])
         if term.is_compound:
-            return cons(term.connector, *[self.logic(t, rule) for t in term.terms])
+            return cons(term.connector, *[self.logic(t, rule, substitution) for t in term.terms])
 
     def term(self, logic):
         if type(logic) is Term:
@@ -181,27 +234,63 @@ class KanrenEngine:
     def _diff(self, c):
         difference = -1 # result of applying diff
 
-        if type(c) is Statement:
-            if c.subject.is_compound and c.copula is Copula.Implication:
-                if c.subject.connector is Connector.ExtensionalDifference:
-                    if len(c.subject.terms.terms) == 2:
-                        components = c.subject.terms.terms
-                        l = components[0]
-                        r = components[1]
-                        if l.contains(r) and not l.equal(r):
-                            difference = l - r
-                        else:
-                            difference = None
+        def calculate_difference(l: Term, r: Term):
+            return (l - r) if l.contains(r) and not l.equal(r) else None
+            
+        def do_diff(t: Term):
+            nonlocal difference
+            if len(t.terms.terms) == 2:
+                components = t.terms.terms
+                difference = calculate_difference(*components)
 
-        if difference == None or difference == -1:
-            return difference
-        else:
-            return Statement(difference, c.copula, c.predicate)
+
+        # COMPOUND
+        if type(c) is Compound and c.connector is Connector.ExtensionalDifference:
+            if len(c.terms.terms) == 2:
+                return calculate_difference(*c.terms.terms)
+
+        # STATEMENT
+        elif type(c) is Statement and c.copula is Copula.Implication:
+            # check subject
+            subject = c.subject
+            if subject.is_compound:
+                if subject.connector is Connector.ExtensionalDifference:
+                    do_diff(c.subject)
+                    if difference is not None:
+                        subject = difference
+
+                # check for nested difference
+                elif subject.connector is Connector.Conjunction:
+                    if len(subject.terms.terms) == 2:
+                        components = subject.terms.terms
+                        if components[0].is_compound:
+                            if components[0].connector is Connector.ExtensionalDifference:
+                                do_diff(components[0])
+                                # if components[0].terms.terms[0] == components[1]:
+                                #     difference = None
+                                if difference is not None:
+                                    subject = Compound(subject.connector, difference, components[1])
+
+            # check predicate
+            predicate = c.predicate
+            if predicate.is_compound and difference is not None: # already failed one check
+                if predicate.connector is Connector.ExtensionalDifference:
+                    do_diff(predicate)
+                    if difference is not None:
+                        predicate = difference
+
+            # check for success
+            if difference == None or difference == -1:
+                return difference
+            else:
+                return Statement(subject, c.copula, predicate)
+
+        return -1 # no difference was applied
 
     # UNIFICATION
 
     def _variable_elimination(self, t1: Term, t2: Term) -> list:
-        unified = filter(None, (unify(self.logic(t), self.logic(t2, True)) for t in t1.terms))
+        unified = filter(None, (unify(self.logic(t), self.logic(t2, True, True)) for t in t1.terms))
         substitution = []
         for u in unified:
             d = {k: v for k, v in u.items() if type(self.term(k)) is Variable}
@@ -245,6 +334,7 @@ class KanrenEngine:
                 r = r.replace("'", '') # remove trailing '
                 t1t, t2t = (t1.truth, t2.truth) if not inverse else (t2.truth, t1.truth)
                 truth = self._truth_functions[r](t1t, t2t)
+                # results.append(((res, truth), self.term(rule[0][0]), self.term(rule[0][1]), self.term(rule[0][2])))
                 results.append((res, truth))
         return results
 
@@ -267,7 +357,7 @@ class KanrenEngine:
         #     print("Substituted:", t1, t2)
 
         # apply rule
-        result = run(1, c, eq((p1, p2), (self.logic(t1), self.logic(t2))))
+        result = run(1, c, eq((p1, p2), (self.logic(t1), self.logic(t2))), *self.constraints)
         # print(result)
 
         if result:
@@ -302,6 +392,21 @@ print(engine.inference(t1, t2))
 
 t2 = parse('(&&, B, C).') # complex example
 print(engine.inference(t1, t2))
+
+print('\n--NAL 5--')
+
+t2 = parse('<U ==> B>.')
+print(engine.inference(t1, t2))
+
+t2 = parse('<B ==> Z>.')
+# print(engine.inference(t1, t2))
+for r in engine.inference(t1, t2):
+    print(r)
+
+# t2 = parse('<U ==> B>.')
+# print(engine.inference(t1, t2))
+
+print('\n----DEDUCTION')
 
 # DEDUCTION
 
