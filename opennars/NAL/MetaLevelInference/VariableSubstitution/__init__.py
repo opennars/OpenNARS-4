0112e666deeab81cc1bc1a160f9f9a9b385ab5a6
Bowen XU <58252898+bowen-xu@users.noreply.github.com>
2024-11-13 18:28:57 -0500

    remove the old engine

--- opennars/NAL/MetaLevelInference/VariableSubstitution/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-**Variable substitution.** All occurrences of an independent variable term in a statement can be substituted by another term (constant or variable); all occurrences of a term (constant or variable) in a statement can be substituted by a dependent variable term. The reverse cases of these substitution are limited to the cases discussed in NAL-6. A query variable in a question can be substituted by a constant term in a judgment.
-'''
-
-from .Substitution import Substitution
-from .Elimination import Elimination
-from .Introduction import Introduction
-from .Unification import *
